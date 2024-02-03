7d0800618ad387c5075385e6d6c32eb6ab8cc570
concagabriel <58643247+concagabriel@users.noreply.github.com>
2024-02-03 16:05:12 -0300

    Update __manifest__.py

--- sdhit_default/__manifest__.py
+++ sdhit_default/__manifest__.py
@@ -41,6 +41,7 @@
     "git-repos": [
         "https://github.com/sistemasdehudson/cl-sdhit",
         "https://github.com/CybroOdoo/CybroAddons.git -b 17.0",
+        "https://github.com/Yenthe666/auto_backup -b 17.0",
     #localizacion italiana
 #        "https://github.com/OCA/l10n-italy.git -b 17.0",
 #        'https://github.com/OCA/account-financial-reporting.git -b 17.0',
