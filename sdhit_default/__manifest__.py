d0184326640fe1335f16a6c3c8eeb44d9158967f
concagabriel <58643247+concagabriel@users.noreply.github.com>
2024-07-04 10:56:30 +0200

    Update __manifest__.py

--- sdhit_default/__manifest__.py
+++ sdhit_default/__manifest__.py
@@ -75,7 +75,7 @@
     ],
     # list of images to use in the form 'name image-url'
     "docker-images": [
-        "odoo regaby/odoo-ce:16.0",
+        "odoo jobiols/odoo-jeo:16.0",
         "postgres postgres:15.1-alpine",
     ],
 }
